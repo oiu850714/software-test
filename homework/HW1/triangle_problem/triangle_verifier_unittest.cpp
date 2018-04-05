49d40eb467778ceb75c06456d59d6bdbb2c4b6ea
oiu850714 <oiu850714@gmail.com>
2018-04-05 15:42:22 +0800

    add unit test file and normal boundary value tests for triangle problem

---
+++ homework/HW1/triangle_problem/triangle_verifier_unittest.cpp
@@ -0,0 +1,27 @@
+#include "triangle_verifier.h"
+#include "gtest/gtest.h"
+
+#include <string>
+namespace {
+
+// normal boundary value test for triangle problem
+// input domain: [1, 200]
+TEST(TraingleVerifierTest, NormalBoundaryValue) {
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(1, 100, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(2, 100, 100));
+  EXPECT_EQ("Equilateral", triangle::triangle_verifier(100, 100, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(199, 100, 100));
+  EXPECT_EQ("Not a triangle", triangle::triangle_verifier(200, 100, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 1, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 2, 100));
+  // EXPECT_EQ("Equilateral", triangle::triangle_verifier(100, 100, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 199, 100));
+  EXPECT_EQ("Not a triangle", triangle::triangle_verifier(100, 200, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 100, 1));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 100, 2));
+  // EXPECT_EQ("Equilateral", triangle::triangle_verifier(100, 100, 100));
+  EXPECT_EQ("Isosceles", triangle::triangle_verifier(100, 100, 199));
+  EXPECT_EQ("Not a triangle", triangle::triangle_verifier(100, 100, 200));
+}
+
+} // namespace
