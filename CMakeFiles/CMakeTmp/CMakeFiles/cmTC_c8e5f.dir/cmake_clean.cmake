0a58f13c157b4ae6c8c93e4fe96543fbc5057d5f
oiu850714 <oiu850714@gmail.com>
2018-04-05 10:21:55 +0800

    add googletest files to let test buinding more simple

---
+++ CMakeFiles/CMakeTmp/CMakeFiles/cmTC_c8e5f.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_c8e5f.dir/testCCompiler.c.o"
+  "cmTC_c8e5f.pdb"
+  "cmTC_c8e5f"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_c8e5f.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
