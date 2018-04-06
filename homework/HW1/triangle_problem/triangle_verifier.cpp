33563107657d7653f25b115b3b9bb6963f22c63f
oiu850714 <oiu850714@gmail.com>
2018-04-07 00:21:46 +0800

    add implementation for triangle problem

--- homework/HW1/triangle_problem/triangle_verifier.cpp
+++ homework/HW1/triangle_problem/triangle_verifier.cpp
@@ -1,7 +1,24 @@
 #include "triangle_verifier.h"
 #include <string>
 
+#define DOMAIN_LOW 1
+#define DOMAIN_UP 200
+
 std::string triangle::triangle_verifier(int a, int b, int c) {
-  return "Equilateral";
+  if ((a < DOMAIN_LOW || a > DOMAIN_UP) || (b < DOMAIN_LOW || b > DOMAIN_UP) ||
+      (c < DOMAIN_LOW || c > DOMAIN_UP)) {
+    return "Invalid input";
+  }
+
+  if (!(a < b + c && b < c + a && c < a + b)) {
+    return "Not a triangle";
+  }
+
+  if (a == b && b == c) {
+    return "Equilateral";
+  } else if (a != b && b != c && c != a) {
+    return "Scalene";
+  } else
+    return "Isosceles";
 }
 // namespace triangle
