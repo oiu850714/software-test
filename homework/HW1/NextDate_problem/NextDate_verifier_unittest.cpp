8acc8677d1f0b9ffe849f73a0d3ded511b20ba39
oiu850714 <oiu850714@gmail.com>
2018-04-23 00:01:53 +0800

    add weak normal and weak robust boundary valuetest for NextDate problem

---
+++ homework/HW1/NextDate_problem/NextDate_verifier_unittest.cpp
@@ -0,0 +1,113 @@
+#include "NextDate_verifier.h"
+#include "gtest/gtest.h"
+
+#include <string>
+namespace {
+
+// normal boundary value test for NextDate problem
+// input domain: 1812/01/01 - 2012/12/31
+TEST(NextDateVerifierBoundaryValueTest, WeakNormal) {
+  EXPECT_EQ(Date(1812, 7, 16), NextDate_verifier(Date(1812, 7, 15)));
+  EXPECT_EQ(Date(1813, 7, 16), NextDate_verifier(Date(1813, 7, 15)));
+  EXPECT_EQ(Date(1912, 7, 16), NextDate_verifier(Date(1912, 7, 15)));
+  EXPECT_EQ(Date(2011, 7, 16), NextDate_verifier(Date(2011, 7, 15)));
+  EXPECT_EQ(Date(2012, 7, 16), NextDate_verifier(Date(2012, 7, 15)));
+  EXPECT_EQ(Date(1912, 1, 16), NextDate_verifier(Date(1912, 1, 15)));
+  EXPECT_EQ(Date(1912, 2, 16), NextDate_verifier(Date(1912, 2, 15)));
+  EXPECT_EQ(Date(1912, 7, 16), NextDate_verifier(Date(1912, 7, 15)));
+  EXPECT_EQ(Date(1912, 11, 16), NextDate_verifier(Date(1912, 11, 15)));
+  EXPECT_EQ(Date(1912, 12, 16), NextDate_verifier(Date(1912, 12, 15)));
+  EXPECT_EQ(Date(1912, 7, 2), NextDate_verifier(Date(1912, 7, 1)));
+  EXPECT_EQ(Date(1912, 7, 3), NextDate_verifier(Date(1912, 7, 2)));
+  EXPECT_EQ(Date(1912, 7, 16), NextDate_verifier(Date(1912, 7, 15)));
+  EXPECT_EQ(Date(1912, 7, 31), NextDate_verifier(Date(1912, 7, 30)));
+  EXPECT_EQ(Date(1912, 8, 1), NextDate_verifier(Date(1912, 7, 31)));
+}
+
+// invalid(robust) input test for boundary value test for NextDate problem
+// input domain: 1812/01/01 - 2012/12/31
+TEST(NextDateVerifierBoundaryValueTest, WeakRobust) {
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(1811, 7, 15)));
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(2013, 7, 15)));
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(1912, 0, 15)));
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(1912, 13, 15)));
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(1912, 7, 0)));
+  EXPECT_EQ(DATE_INVALID, NextDate_verifier(Date(1912, 7, 32)));
+}
+
+// no strong normal and strong robust test cases for boundary value tesr for
+// NextDate problem because it is too tedious
+
+// random input test for boundary value test for triangle problem
+// input domain: 1812/01/01 - 2012/12/31
+TEST(NextDateVerifierBoundaryValueTest, Random) {
+  EXPECT_EQ(Date(1950, 7, 1), NextDate_verifier(Date(1950, 6, 30)));
+  EXPECT_EQ(Date(2000, 2, 29), NextDate_verifier(Date(2000, 2, 28)));
+}
+
+// Equivalence class test for triangle problem
+//
+//   Equilateral
+//   Isosceles
+//   Scalene
+//   Not a triangle
+
+/*
+// weak normal
+TEST(NextDateVerifierEquivalenceClassTest, WeakNormal) {
+  EXPECT_EQ("Equilateral", NextDate_verifier(5, 5, 5));
+  EXPECT_EQ("Isosceles", NextDate_verifier(50, 80, 80));
+  EXPECT_EQ("Scalene", NextDate_verifier(3, 4, 5));
+  EXPECT_EQ("Not a triangle", NextDate_verifier(1, 2, 3));
+}
+
+// strong normal
+// Because no valid subintervals of either side length exist, the strong normal
+// equivalence class test cases are identical to the weak normal equivalence
+// test cases.
+
+// weak robust
+TEST(NextDateVerifierEquivalenceClassTest, WeakRobust) {
+  EXPECT_EQ("Invalid input", NextDate_verifier(-1, 5, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, -1, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, 5, -1));
+  EXPECT_EQ("Invalid input", NextDate_verifier(201, 5, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, 201, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, 5, 201));
+}
+
+// strong robust
+TEST(NextDateVerifierEquivalenceClassTest, StrongRobust) {
+  EXPECT_EQ("Invalid input", NextDate_verifier(-1, 5, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, -1, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(5, 5, -1));
+  EXPECT_EQ("Invalid input", NextDate_verifier(-1, -1, 5));
+  EXPECT_EQ("Invalid input", NextDate_verifier(-1, 5, -1));
+  EXPECT_EQ("Invalid input", NextDate_verifier(-1, -1, -1));
+}
+
+// decision table method
+// refer to the ppt of chpater 07, page 7 and 22
+TEST(NextDateVerifierDecisionTableTest, ALL) {
+  EXPECT_EQ("Not a triangle", NextDate_verifier(4, 1, 2));
+  EXPECT_EQ("Not a triangle", NextDate_verifier(1, 4, 2));
+  EXPECT_EQ("Not a triangle", NextDate_verifier(1, 2, 4));
+  EXPECT_EQ("Equilateral", NextDate_verifier(5, 5, 5));
+
+  // EXPECT_EQ("Invalid input", NextDate_verifier(, , ));
+  // impossible
+
+  // EXPECT_EQ("Invalid input", NextDate_verifier(, , ));
+  // impossible
+
+  EXPECT_EQ("Isosceles", NextDate_verifier(2, 2, 3));
+
+  // EXPECT_EQ("Invalid input", NextDate_verifier(, , ));
+  // impossible
+
+  EXPECT_EQ("Isosceles", NextDate_verifier(2, 3, 2));
+  EXPECT_EQ("Isosceles", NextDate_verifier(3, 2, 2));
+  EXPECT_EQ("Scalene", NextDate_verifier(3, 4, 5));
+}
+*/
+} // namespace
