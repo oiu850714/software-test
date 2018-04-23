55e6f8439e8edf20dd4af0e1f499bc15016a6881
oiu850714 <oiu850714@gmail.com>
2018-04-23 19:38:55 +0800

    add const qualifier to operator==

--- homework/HW1/Commission_problem/Commission_verifier.h
+++ homework/HW1/Commission_problem/Commission_verifier.h
@@ -3,7 +3,7 @@
 class Commission {
 public:
   Commission(int s, double c) : sales(s), commission(c) {}
-  bool operator==(const Commission &com) {
+  bool operator==(const Commission &com) const {
     if (com.sales == sales && com.commission == commission)
       return true;
     else
@@ -15,5 +15,6 @@ private:
   double commission;
 };
 
+const Commission INVALID_COMMISSION(-1, -1);
 Commission Commission_verifier(const int Locks, const int Stocks,
                                const int Barrels);
