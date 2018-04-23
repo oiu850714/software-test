15f1da77cb434fe5153e4454fd22977bd706d1a8
oiu850714 <oiu850714@gmail.com>
2018-04-23 19:54:55 +0800

    add implementation for checking invalid input for Commission problem

--- homework/HW1/Commission_problem/Commission_verifier.cpp
+++ homework/HW1/Commission_problem/Commission_verifier.cpp
@@ -2,5 +2,13 @@
 
 Commission Commission_verifier(const int Locks, const int Stocks,
                                const int Barrels) {
+  const int LockPrice = 45, StockPrice = 30, BarrelPrice = 25;
+  if (Locks < 1 || Locks > 70)
+    return Commission(-1, -1);
+  if (Stocks < 1 || Stocks > 80)
+    return Commission(-1, -1);
+  if (Barrels < 1 || Barrels > 90)
+    return Commission(-1, -1);
+
   return Commission(0, 0);
 }
