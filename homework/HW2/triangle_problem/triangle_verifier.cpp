6199ae7927c8455f555d1ce4062e0e99f16e7dd4
oiu850714 <oiu850714@gmail.com>
2018-05-06 16:09:57 +0800

    add some comments to help clarify test coverage

--- homework/HW2/triangle_problem/triangle_verifier.cpp
+++ homework/HW2/triangle_problem/triangle_verifier.cpp
@@ -5,15 +5,19 @@
 #define DOMAIN_UP 200
 
 std::string triangle::triangle_verifier(int a, int b, int c) {
+
+  // 1st if
   if ((a < DOMAIN_LOW || a > DOMAIN_UP) || (b < DOMAIN_LOW || b > DOMAIN_UP) ||
       (c < DOMAIN_LOW || c > DOMAIN_UP)) {
     return "Invalid input";
   }
 
+  // 2nd if
   if (!(a < b + c && b < c + a && c < a + b)) {
     return "Not a triangle";
   }
 
+  // 3rd if
   if (a == b && b == c) {
     return "Equilateral";
   } else if (a != b && b != c && c != a) {
