7615af0332826ab479cc70626c49c8f94a270910
oiu850714 <oiu850714@gmail.com>
2018-05-06 16:14:46 +0800

    add C1 test for triangle problem

--- homework/HW2/triangle_problem/triangle_verifier_pathtest.cpp
+++ homework/HW2/triangle_problem/triangle_verifier_pathtest.cpp
@@ -18,3 +18,21 @@ TEST(TriangleVerifier, C0) {
   ASSERT_EQ("Isosceles", triangle::triangle_verifier(2, 2, 3));
   ASSERT_EQ("Scalene", triangle::triangle_verifier(3, 4, 5));
 }
+
+// DD-path coverage
+TEST(TriangleVerifier, C1) {
+  // 1st if is true, and return
+  ASSERT_EQ("Invalid input", triangle::triangle_verifier(0, 0, 0));
+
+  // 1st if is false, 2nd if is true, and return
+  ASSERT_EQ("Not a triangle", triangle::triangle_verifier(1, 2, 3));
+
+  // 1st and 2nd if are false, and 3rd if is true, and return
+  ASSERT_EQ("Equilateral", triangle::triangle_verifier(5, 5, 5));
+
+  // 1st and 2nd if are false, and 3rd "else part" is true, and return
+  ASSERT_EQ("Isosceles", triangle::triangle_verifier(2, 2, 3));
+
+  // 1st and 2nd if are false, and 3rd "else if part" is true, and return
+  ASSERT_EQ("Scalene", triangle::triangle_verifier(3, 4, 5));
+}
