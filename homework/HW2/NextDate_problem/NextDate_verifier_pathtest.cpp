3ecad31a532ecdc0e6254d9d792009fea69d6f14
oiu850714 <oiu850714@gmail.com>
2018-05-06 22:57:13 +0800

    add C1 and C2 test for NextDate problem

--- homework/HW2/NextDate_problem/NextDate_verifier_pathtest.cpp
+++ homework/HW2/NextDate_problem/NextDate_verifier_pathtest.cpp
@@ -12,5 +12,51 @@ TEST(NextDateVerifier, C0) {
   // executes remaining branches
   ASSERT_EQ(Date(2001, 01, 01), NextDate_verifier(Date(2000, 12, 31)));
 }
+TEST(NextDateVerifier, C1) {
+
+  // 執行 1st "big" if
+  ASSERT_EQ(DATE_INVALID, NextDate_verifier(Date(1811, 13, 30)));
+
+  // 2nd if: 大月跟小月，還有二月
+  ASSERT_EQ(Date(2000, 01, 16), NextDate_verifier(Date(2000, 01, 15)));
+  ASSERT_EQ(Date(2000, 04, 16), NextDate_verifier(Date(2000, 04, 15)));
+  ASSERT_EQ(Date(2000, 02, 16), NextDate_verifier(Date(2000, 02, 15)));
+
+  // 3rd if: 閏年跟非閏年
+  ASSERT_EQ(Date(2000, 02, 29), NextDate_verifier(Date(2000, 02, 28)));
+  ASSERT_EQ(Date(2001, 03, 01), NextDate_verifier(Date(2001, 02, 28)));
+
+  // 4th if:
+  // 非月底
+  ASSERT_EQ(Date(2000, 06, 16), NextDate_verifier(Date(2000, 06, 15)));
+
+  // 月底，非 12 月跟 12 月
+  ASSERT_EQ(Date(2000, 07, 01), NextDate_verifier(Date(2000, 06, 30)));
+  ASSERT_EQ(Date(2001, 01, 01), NextDate_verifier(Date(2000, 12, 31)));
+}
+
+// No loop, same as C1
+TEST(NextDateVerifier, C2) {
+
+  // 執行 1st "big" if
+  ASSERT_EQ(DATE_INVALID, NextDate_verifier(Date(1811, 13, 30)));
+
+  // 2nd if: 大月跟小月，還有二月
+  ASSERT_EQ(Date(2000, 01, 16), NextDate_verifier(Date(2000, 01, 15)));
+  ASSERT_EQ(Date(2000, 04, 16), NextDate_verifier(Date(2000, 04, 15)));
+  ASSERT_EQ(Date(2000, 02, 16), NextDate_verifier(Date(2000, 02, 15)));
+
+  // 3rd if: 閏年跟非閏年
+  ASSERT_EQ(Date(2000, 02, 29), NextDate_verifier(Date(2000, 02, 28)));
+  ASSERT_EQ(Date(2001, 03, 01), NextDate_verifier(Date(2001, 02, 28)));
+
+  // 4th if:
+  // 非月底
+  ASSERT_EQ(Date(2000, 06, 16), NextDate_verifier(Date(2000, 06, 15)));
+
+  // 月底，非 12 月跟 12 月
+  ASSERT_EQ(Date(2000, 07, 01), NextDate_verifier(Date(2000, 06, 30)));
+  ASSERT_EQ(Date(2001, 01, 01), NextDate_verifier(Date(2000, 12, 31)));
+}
 
 } // namespace
