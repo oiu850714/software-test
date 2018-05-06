efffa44ee6bc2ad92e59dd84dbc62aa3c122e9d9
oiu850714 <oiu850714@gmail.com>
2018-05-06 23:43:13 +0800

    add C1 and C2 test for Commission problem

--- homework/HW2/Commission_problem/Commission_verifier_pathtest.cpp
+++ homework/HW2/Commission_problem/Commission_verifier_pathtest.cpp
@@ -17,4 +17,30 @@ TEST(CommissionVerifier, C0) {
   EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
 }
 
+TEST(CommissionVerifier, C1) {
+
+  // 執行 1st "big" if 的 所有 true/false
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(0, 1, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 0, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 1, 0));
+
+  // 執行 2nd if 的 statements
+  EXPECT_EQ(Commission(100, 10), Commission_verifier(1, 1, 1));
+  EXPECT_EQ(Commission(1025, 103.75), Commission_verifier(10, 10, 11));
+  EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
+}
+
+TEST(CommissionVerifier, C2) {
+
+  // 執行 1st "big" if 的 所有 true/false
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(0, 1, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 0, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 1, 0));
+
+  // 執行 2nd if 的 statements
+  EXPECT_EQ(Commission(100, 10), Commission_verifier(1, 1, 1));
+  EXPECT_EQ(Commission(1025, 103.75), Commission_verifier(10, 10, 11));
+  EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
+}
+
 } // namespace
