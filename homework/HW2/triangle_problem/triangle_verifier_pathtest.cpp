e9974146b3451066297d0ceac25e59d2259a3f36
oiu850714 <oiu850714@gmail.com>
2018-05-06 16:34:01 +0800

    add MCDC test for triangle problem

--- homework/HW2/triangle_problem/triangle_verifier_pathtest.cpp
+++ homework/HW2/triangle_problem/triangle_verifier_pathtest.cpp
@@ -55,3 +55,22 @@ TEST(TriangleVerifier, C2) {
   // 1st and 2nd if are false, and 3rd "else if part" is true, and return
   ASSERT_EQ("Scalene", triangle::triangle_verifier(3, 4, 5));
 }
+
+// MC/DC
+// all conditions that can affect the decision outcome independently
+TEST(TriangleVerifier, MCDC) {
+  // 1st branch
+  ASSERT_EQ("Invalid input", triangle::triangle_verifier(201, 5, 5));
+  ASSERT_EQ("Invalid input", triangle::triangle_verifier(5, 201, 5));
+  ASSERT_EQ("Invalid input", triangle::triangle_verifier(5, 5, 201));
+  ASSERT_EQ("Equilateral", triangle::triangle_verifier(5, 5, 5));
+  // 2nd branch
+  ASSERT_EQ("Not a triangle", triangle::triangle_verifier(1, 2, 3));
+  ASSERT_EQ("Not a triangle", triangle::triangle_verifier(3, 2, 1));
+  ASSERT_EQ("Not a triangle", triangle::triangle_verifier(2, 3, 1));
+  // 3rd branch
+  ASSERT_EQ("Isosceles", triangle::triangle_verifier(2, 2, 3));
+  ASSERT_EQ("Isosceles", triangle::triangle_verifier(2, 3, 2));
+  ASSERT_EQ("Isosceles", triangle::triangle_verifier(3, 2, 2));
+  ASSERT_EQ("Scalene", triangle::triangle_verifier(3, 4, 5));
+}
