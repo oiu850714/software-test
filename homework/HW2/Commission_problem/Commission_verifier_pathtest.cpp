4172f589022ad96486cffcacc15bdf2be5481d10
oiu850714 <oiu850714@gmail.com>
2018-05-06 23:47:35 +0800

    add MCDC test for Commission problem

--- homework/HW2/Commission_problem/Commission_verifier_pathtest.cpp
+++ homework/HW2/Commission_problem/Commission_verifier_pathtest.cpp
@@ -43,4 +43,20 @@ TEST(CommissionVerifier, C2) {
   EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
 }
 
+TEST(CommissionVerifier, MCDC) {
+
+  // 執行 1st "big" if
+  // 把所有會獨立影響 if statements 的 true/false 的 condition 都考慮進去
+  EXPECT_EQ(Commission(100, 10), Commission_verifier(1, 1, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(0, 1, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 0, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 1, 0));
+
+  // 執行 2nd if 的 statements，這邊只有一個 condition 組成整個 if statements
+  // 的條件，故 test case 跟 C1 的一樣
+  EXPECT_EQ(Commission(100, 10), Commission_verifier(1, 1, 1));
+  EXPECT_EQ(Commission(1025, 103.75), Commission_verifier(10, 10, 11));
+  EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
+}
+
 } // namespace
