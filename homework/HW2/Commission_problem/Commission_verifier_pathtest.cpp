192c6a08115217df207bc083a58f216d51f541a1
oiu850714 <oiu850714@gmail.com>
2018-05-06 23:42:21 +0800

    add C0 test for Commission problem

---
+++ homework/HW2/Commission_problem/Commission_verifier_pathtest.cpp
@@ -0,0 +1,20 @@
+#include "Commission_verifier.h"
+#include "gtest/gtest.h"
+
+#include <string>
+namespace {
+
+TEST(CommissionVerifier, C0) {
+
+  // 執行 1st "big" if 的 statements
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(0, 1, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 0, 1));
+  EXPECT_EQ(INVALID_COMMISSION, Commission_verifier(1, 1, 0));
+
+  // 執行 2nd if 的 statements
+  EXPECT_EQ(Commission(100, 10), Commission_verifier(1, 1, 1));
+  EXPECT_EQ(Commission(1025, 103.75), Commission_verifier(10, 10, 11));
+  EXPECT_EQ(Commission(1825, 225), Commission_verifier(18, 18, 19));
+}
+
+} // namespace
