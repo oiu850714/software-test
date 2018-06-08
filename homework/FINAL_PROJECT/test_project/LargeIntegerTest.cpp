88a854e29c818efe2be4893949b0ec3686733473
oiu850714 <oiu850714@gmail.com>
2018-06-08 11:31:40 +0800

    add tests for LargeInteger ctors

--- homework/FINAL_PROJECT/test_project/LargeIntegerTest.cpp
+++ homework/FINAL_PROJECT/test_project/LargeIntegerTest.cpp
@@ -1,270 +1,126 @@
 #include "LargeInteger.h"
 #include "gtest/gtest.h"
-
-/*
-
-  LargeInteger.h
-  Unsigned large integer class.
-
-
-  Note: the number stored in this class is in little-endian binary. And all
-        the binary data stored in the memory is considered encoded in LE too.
-        If your system is a BE system, please rewrite this library.
-
-        Fixed-width, little-endian binary, dynamiclly allocated.
-
-  Designer:    cin.getline
-  Last update: 2012-07-21
-
-*/
-
-// #ifndef _CIN_GETLINE_LARGE_INTEGER_H__
-// #define _CIN_GETLINE_LARGE_INTEGER_H__
-
-// #ifndef __cplusplus
-// #error *** This is a C++ library! ***
-// #endif
-
-// #include "TypeDefinition.h"
-
-// #define DIVISION_BY_ZERO 1
-// #define MODULO_BY_ZERO 2
-// #define INVALID_RADIX 3
-// #define ILLEGAL_CHARACTER 4
-
-// // Declartion.
-
-// // BitLength indicates the minimun number of bits that this class can store.
-// template <size_t BitLength> class LargeInteger {
-
-//   // Interface.
-// public:
-//   // Constructors.
-//   LargeInteger();
-//   LargeInteger(const LargeInteger<BitLength> &RHS);
-//   LargeInteger(LargeInteger<BitLength> &&RHS);
-//   LargeInteger(QWORD Number);
-
-//   // Construct from other bit length.
-//   template <size_t RHSBitLength>
-//   explicit LargeInteger(const LargeInteger<RHSBitLength> &RHS);
-
-//   // Destructor.
-//   ~LargeInteger();
-
-//   // Bitwise operators.
-//   LargeInteger<BitLength> &operator>>=(int NumberToShift);
-//   LargeInteger<BitLength> &operator<<=(int NumberToShift);
-//   LargeInteger<BitLength> &operator&=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator|=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator^=(const LargeInteger<BitLength> &RHS);
-
-//   LargeInteger<BitLength> operator~() const;
-//   LargeInteger<BitLength> operator>>(int NumberToShift) const;
-//   LargeInteger<BitLength> operator<<(int NumberToShift) const;
-
-//   // Comparison operators.
-//   bool operator==(const LargeInteger<BitLength> &RHS) const;
-//   bool operator!=(const LargeInteger<BitLength> &RHS) const;
-//   bool operator>=(const LargeInteger<BitLength> &RHS) const;
-//   bool operator<=(const LargeInteger<BitLength> &RHS) const;
-//   bool operator>(const LargeInteger<BitLength> &RHS) const;
-//   bool operator<(const LargeInteger<BitLength> &RHS) const;
-
-//   // Convertions.
-//   explicit operator bool() const;
-//   explicit operator QWORD() const;
-//   explicit operator DWORD() const;
-
-//   // Assignment operators.
-//   LargeInteger<BitLength> &operator=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator=(LargeInteger<BitLength> &&RHS);
-//   LargeInteger<BitLength> &operator=(QWORD Number);
-
-//   // Arithmetic operators.
-//   LargeInteger<BitLength> &operator+=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator-=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator*=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator/=(const LargeInteger<BitLength> &RHS);
-//   LargeInteger<BitLength> &operator%=(const LargeInteger<BitLength> &RHS);
-
-//   LargeInteger<BitLength> operator+() const;
-//   LargeInteger<BitLength> operator-() const;
-//   LargeInteger<BitLength> operator++(int);
-//   LargeInteger<BitLength> &operator++();
-//   LargeInteger<BitLength> operator--(int);
-//   LargeInteger<BitLength> &operator--();
-
-//   // Other member functions.
-
-//   // I/O data without casting.
-//   void GetRawData(void *OutputDataBuffer) const;
-//   void SetRawData(const void *InputDataBuffer);
-
-//   // I/O via a string.
-//   const char *ConvertToString(unsigned int Radix) const;
-//   void SetFromString(const char *StrNumber, unsigned int Radix);
-
-//   // Presented in byte.
-//   size_t GetBufferSize(void) const;
-
-//   // Private members.
-// private:
-//   QWORD *NumberCell;
-//   static const int QWORDLength =
-//       (BitLength > 0) ? ((BitLength / 64) + ((BitLength % 64) ? 1 : 0)) : 1;
-
-//   // Output functions used by the public function ConvertToString.
-//   void RegularConvertToString(unsigned int Radix, char *StrToWrite) const;
-//   void PowerOfTwoConvertToString(unsigned int Radix, char *StrToWrite) const;
-
-//   // Input functions used by the public function SetFromString.
-//   void RegularSetFromString(const char *StrNumber, unsigned int Radix);
-//   void PowerOfTwoSetFromString(const char *StrNumber, unsigned int Radix);
-
-//   // Friend declaration.
-//   template <size_t OtherBitLength> friend class LargeInteger;
-// };
-
-// // Arthematic operator declaration.
-
-// // Opertor+
-// template <size_t Length>
-// LargeInteger<Length> operator+(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator+(const LargeInteger<Length> &LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator+(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator+(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator-
-// template <size_t Length>
-// LargeInteger<Length> operator-(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> operator-(const LargeInteger<Length> &LHS,
-//                                LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator-(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator-(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator*
-// template <size_t Length>
-// LargeInteger<Length> operator*(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator*(const LargeInteger<Length> &LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator*(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator*(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator/
-// template <size_t Length>
-// LargeInteger<Length> operator/(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> operator/(const LargeInteger<Length> &LHS,
-//                                LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator/(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator/(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator%
-// template <size_t Length>
-// LargeInteger<Length> operator%(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> operator%(const LargeInteger<Length> &LHS,
-//                                LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator%(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator%(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Bitwise operator declaration.
-
-// // Operator&
-// template <size_t Length>
-// LargeInteger<Length> operator&(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator&(const LargeInteger<Length> &LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator&(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator&(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator|
-// template <size_t Length>
-// LargeInteger<Length> operator|(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator|(const LargeInteger<Length> &LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator|(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator|(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// // Operator^
-// template <size_t Length>
-// LargeInteger<Length> operator^(const LargeInteger<Length> &LHS,
-//                                const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator^(const LargeInteger<Length> &LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator^(LargeInteger<Length> &&LHS,
-//                                  const LargeInteger<Length> &RHS);
-
-// template <size_t Length>
-// LargeInteger<Length> &&operator^(LargeInteger<Length> &&LHS,
-//                                  LargeInteger<Length> &&RHS);
-
-// #include "LargeInteger.cpp"
-// #endif
+#include <iostream>
+#include <utility>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+namespace
+{
+
+
+TEST(LargeIntegerCoverageLOL, LOL){
+  //LargeInteger<6401> L1(9487);
+}
+
+
+class LargeIntegerConstructorTest : public testing::Test
+{
+protected:
+  
+  // naming convention: L_<BitLength>_<value>_<radix>
+  LargeInteger <6400> L_6400_0_10;
+  const QWORD* L_6400_0_10_internalBuffer;
+  int L_6400_0_10_internalBufferLength;
+  virtual void SetUp() {
+    L_6400_0_10_internalBuffer = L_6400_0_10.GetInternalBuffer();
+    L_6400_0_10_internalBufferLength = L_6400_0_10.GetInternalBufferLength();
+  }
+};
+
+
+TEST_F(LargeIntegerConstructorTest, DefaultConstructor){
+  for(int i = 0; i < L_6400_0_10_internalBufferLength; i++){
+    EXPECT_EQ(0, L_6400_0_10_internalBuffer[i]);
+  }
+}
+
+TEST_F(LargeIntegerConstructorTest, CopyConstructor){
+  LargeInteger <6400> CoppiedToLargeInteger(L_6400_0_10);
+  auto InternalBuffer = CoppiedToLargeInteger.GetInternalBuffer();
+  auto InternalBufferLength = CoppiedToLargeInteger.GetInternalBufferLength();
+  // buffer in new object should be different from original object
+  ASSERT_NE(L_6400_0_10_internalBuffer, InternalBuffer);
+
+  // buffer length should be equal
+  ASSERT_EQ(L_6400_0_10_internalBufferLength, InternalBufferLength);
+
+  // content should be equal
+  for(int i = 0; i < L_6400_0_10_internalBufferLength; i++){
+    EXPECT_EQ(InternalBuffer[i], L_6400_0_10_internalBuffer[i]);
+  }
+}
+
+TEST_F(LargeIntegerConstructorTest, MoveConstructor){
+  auto original_buffer = L_6400_0_10_internalBuffer;
+  LargeInteger <6400> MovedToLargeInteger(std::move(L_6400_0_10));
+  auto InternalBuffer = MovedToLargeInteger.GetInternalBuffer();
+  auto InternalBufferLength = MovedToLargeInteger.GetInternalBufferLength();
+  
+  // the original object's internal buffer should be moved
+  ASSERT_EQ(L_6400_0_10.GetInternalBuffer(), nullptr);
+
+  // new object's internal buffer should be the same with original buffer
+  ASSERT_EQ(InternalBuffer, original_buffer);
+
+  // buffer length should be equal
+  ASSERT_EQ(InternalBufferLength, L_6400_0_10_internalBufferLength);
+
+  // content should be equal
+  for(int i = 0; i < InternalBufferLength; i++){
+    EXPECT_EQ(0, InternalBuffer[i]);
+  }
+}
+
+TEST_F(LargeIntegerConstructorTest, QWORDConstructor){
+  QWORD num = 0x0123456789ABCDEF;
+  LargeInteger<6400> L(num);
+
+  auto InternalBuffer = L.GetInternalBuffer();
+  auto InternalBufferLength = L.GetInternalBufferLength();
+
+  // all except first QWORD should be 0; first QWORD should be equal to num
+  EXPECT_EQ(num, InternalBuffer[0]);
+  for(int i = 1; i < InternalBufferLength; i++){
+    EXPECT_EQ(0, InternalBuffer[i]);
+  }
+}
+
+TEST_F(LargeIntegerConstructorTest, ConstructorFromDifferentLength){
+
+  auto InternalBuffer = L_6400_0_10.GetInternalBuffer();
+  auto InternalBufferLength = L_6400_0_10.GetInternalBufferLength();
+
+  //overwritting internal binary data lol  
+  for(int i = 0; i < InternalBufferLength; i++){
+    InternalBuffer[i] = i;
+  }
+
+  LargeInteger<3200> L_3200(L_6400_0_10);
+  LargeInteger<12800> L_12800(L_6400_0_10);
+  
+  auto L_3200_InternalBuffer = L_3200.GetInternalBuffer();
+  auto L_3200_InternalBufferLength = L_3200.GetInternalBufferLength();
+  auto L_12800_InternalBuffer = L_12800.GetInternalBuffer();
+  auto L_12800_InternalBufferLength = L_12800.GetInternalBufferLength();
+
+  // case 1: smaller LargeInteger
+  for(int i = 0; i < L_3200_InternalBufferLength; i++){
+    EXPECT_EQ(L_3200_InternalBuffer[i], i);
+  }
+
+  // case 2: larger LargeInteger
+  for(int i = 0; i < InternalBufferLength; i++){
+    EXPECT_EQ(L_12800_InternalBuffer[i], i);
+  }
+  for(int i = InternalBufferLength; i < L_12800_InternalBufferLength; i++){
+    EXPECT_EQ(L_12800_InternalBuffer[i], 0);
+  }
+}
+
+} // namespace
